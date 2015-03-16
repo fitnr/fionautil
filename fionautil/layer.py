d17e01b048c75a4a10529a3ee4613395394cd40d
fitnr <fitnr@fakeisthenewreal>
2015-03-16 11:00:02 -0400

    avoid segfault => iters yield

--- fionautil/layer.py
+++ fionautil/layer.py
@@ -1,22 +1,35 @@
+from __future__ import print_function
 import itertools
 import fiona
+import sys
 
 
-def ffilter(func, filename):
-    with fiona.drivers():
-        with fiona.open(filename, "r") as layer:
-            return itertools.ifilter(func, layer)
+def fionaiter(iterfunc):
+    def _iter(func, filename, *args):
+        with fiona.drivers():
+            with fiona.open(filename, "r") as layer:
+                for feature in iterfunc(func, layer, *args):
+                    yield feature
+    return _iter
 
 
-def fmap(filename, func):
-    '''Add properties to a fiona layer as its read.
-    prop_func takes the feature as an argument and should return it with edited properties/geometry'''
-    with fiona.drivers():
-        with fiona.open(filename, "r") as layer:
-            return itertools.imap(func, layer)
+@fionaiter
+def ffilter(func, layer):
+    return iter(f for f in layer if func(f))
 
 
-def freduce(filename, func, initializer=None):
+@fionaiter
+def ffilterfalse(func, layer):
+    return iter(f for f in layer if not func(f))
+
+
+@fionaiter
+def fmap(func, layer):
+    '''Yield properties in a fiona layer, applying func to each'''
+    return iter(func(f) for f in layer)
+
+
+def freduce(func, filename, initializer=None):
     '''Reduce features of a layer to a single value'''
     with fiona.drivers():
         with fiona.open(filename, "r") as layer:
@@ -31,6 +44,36 @@ def fchain(*filenames):
                 for feature in layer:
                     yield feature
 
+
+def fslice(filename, start, stop=None, step=None):
+    if stop is None:
+        stop = start
+        start = None
+
+    it = iter(xrange(start or 0, stop or sys.maxint, step or 1))
+    nexti = next(it)
+
+    with fiona.drivers():
+        with fiona.open(filename, "r") as layer:
+            for i, element in enumerate(layer):
+                if i == nexti:
+                    yield element
+                    nexti = next(it)
+
+
+def fzip(*filenames):
+    with fiona.drivers():
+        try:
+            handles = [fiona.open(f) for f in filenames]
+            for features in zip(*handles):
+                yield features
+
+        finally:
+            for h in handles:
+                h.close()
+
+
 def find(filename, key, value):
     '''Special case of ffilter: return the first feature where key==value'''
-    return ffilter(lambda f: f['properties'][key] == value, filename).pop()
+    test = lambda f: f['properties'][key] == value
+    return next(ffilter(test, filename))
