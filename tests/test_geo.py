4f74645c40e080cbdbeaa59ddf068770649da7c1
fitnr <fitnr@fakeisthenewreal>
2015-03-25 09:23:39 -0400

    start geo testcase

---
+++ tests/test_geo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of fionautil.
+# http://github.com/fitnr/fionautil
+
+# Licensed under the GPLv3 license:
+# http://www.opensource.org/licenses/GPLv3-license
+# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
+
+from unittest import TestCase
+import unittest.main
+import os.path
+import fionautil.geo
+
+class TestGeo(TestCase):
+
+    def test_turnto(self):
+        pass
+
+
+
+    def test_pointto(self):
+        pass
