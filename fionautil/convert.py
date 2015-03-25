4f5bd6ff1d77d46f222cb7ff5513cd4e4511d6af
fitnr <fitnr@fakeisthenewreal>
2015-03-25 00:46:11 -0400

    remove non-util util file

--- fionautil/convert.py
+++ fionautil/convert.py
@@ -1,4 +1,5 @@
 import math
+import cmath
 
 def rect(angle, length):
     '''polar to cartesian coordinates'''
@@ -7,3 +8,8 @@ def rect(angle, length):
 
 def polar(x, y):
     return math.hypot(x, y), math.atan2(y, x)
+
+
+def angle_to_complex(angle):
+    '''Convert radian clockwise angle to a complex vector of len 1'''
+    return cmath.rect(1, -angle)
